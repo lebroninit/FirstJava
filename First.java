7716cb79cd329ee94d61ae0d76ff5eea475273f3
lebroninit <shaw.andrew.j@gmail.com>
2021-09-27 12:00:01 -0700

    initial commit

---
+++ First.java
@@ -0,0 +1,9 @@
+
+
+public class First {
+    public static void main(String[] args) {
+        System.out.println("My name is Andrew Shaw");
+        System.out.println("I am 18 years old");
+        System.out.println("My hometown is Longmont, CO");
+    }
+}
